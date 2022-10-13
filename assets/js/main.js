ace63ce0e1edb441c51c8c75464e7e382eae6115
Edward Espinoza <edwesti2710@gmail.com>
2022-10-12 22:46:19 -0500

    New Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
-let btnPortabilidad = document.querySelector('.btnAnalitica');
+let btnInicio = document.querySelector('.btnInicio');
+let btnPortabilidad = document.querySelector('.btnPortabilidad');
+let btnDispositivos = document.querySelector('.btnDispositivos');
 let rootContent = document.querySelector('.root');
-btnPortabilidad.addEventListener('click', function(e) {
-    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
-    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
-    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
+
+btnPortabilidad.addEventListener('click', function (e) {
+    rootContent.innerHTML = `            <div class="portabilidad">
+    <iframe src="https://aplicaciones.claro.com.pe/ClienteLineasWeb/consultar.htm" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
+    <iframe src="https://mi.entel.pe/portal/server.pt?&uuID={8650ACC7-3803-625E-B1C8-86A46A111000}&parentname=CommunityPage&parentid=113&mode=2&in_hi_userid=200&cached=false" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
+    <iframe name="bitel" class="bitel" src="https://portal.bitel.com.pe/asistencia/consulta-linea" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms"></iframe>
+</div>`;
+})
+btnInicio.addEventListener('click', function (e) {
+    rootContent.innerHTML = `<h1>Plataforma en Construcción</h1>`;
 })
