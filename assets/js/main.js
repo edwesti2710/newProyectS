7d36db186b657e1dff9e95aad638ad90a0528255
Edward Espinoza <edwesti2710@gmail.com>
2022-10-12 22:23:16 -0500

    New Version

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let btnPortabilidad = document.querySelector('.btnAnalitica');
+let rootContent = document.querySelector('.root');
+btnPortabilidad.addEventListener('click', function(e) {
+    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
+    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
+    rootContent.innerHTML = `<iframe src="./stats.html" frameborder="0"></iframe>`;
+})
