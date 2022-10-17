23a9208fa26d706a380f659d434bc218bb7e09fe
Edward Espinoza <edwesti2710@gmail.com>
2022-10-16 23:48:29 -0500

    update

--- assets/js/devices.js
+++ assets/js/devices.js
@@ -2,25 +2,37 @@ let inputBusqueda = document.getElementById('iSearch');
 inputBusqueda.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
         let device = encodeURI(inputBusqueda.value)
+        console.log(device);
         getData();
         async function getData() {
-            const dataApi = await fetch(`https://phone-specs-api.azharimm.dev/v2/search?query=${device}`);
+            const dataApi = await fetch(`https://sicsystem-devices.cyclic.app/search/${device}`);
+            console.log(`https://sicsystem-devices.cyclic.app/search/${device}`);
+
             const dataJson = await dataApi.json();
-            console.log(dataJson.data.phones);
-            await filterByText(dataJson.data.phones)
+            // data = JSON.parse(dataJson);
+            console.log(dataJson);
+            await filterByText(dataJson)
         }
-        function filterByText(array){
+        function filterByText(array) {
             let htmlData = ''
             array.forEach(element => {
                 console.log(element)
                 htmlData += `            <div class="card">
-                <a href="#">
-                    <img src="${element.image}" alt="" srcset="">
-                    <h3>${element.phone_name}</h3>
+                <a href="#" onclick="getDeviceData('${element.url}')">
+                    <img src="${element.img}" alt="" srcset="">
+                    <h3>${element.name}</h3>
                 </a>
             </div>`
             })
             document.querySelector('.content').innerHTML = htmlData;
         }
+
     }
 })
+async function getDeviceData(device_url) {
+    const dataApi = await fetch(`https://sicsystem-devices.cyclic.app/device/${device_url}`);
+    console.log(`https://sicsystem-devices.cyclic.app/device/${device_url}`);
+    const dataJson = await dataApi.json();
+    // data = JSON.parse(dataJson);
+    console.log(dataJson);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-let btnInicio = document.querySelector('.btnInicio');
-let btnPortabilidad = document.querySelector('.btnPortabilidad');
-let btnDispositivos = document.querySelector('.btnDispositivos');
-let rootContent = document.querySelector('.root');
+// let btnInicio = document.querySelector('.btnInicio');
+// let btnPortabilidad = document.querySelector('.btnPortabilidad');
+// let btnDispositivos = document.querySelector('.btnDispositivos');
+// let rootContent = document.querySelector('.root');
 
-btnPortabilidad.addEventListener('click', function (e) {
-    rootContent.innerHTML = `            <div class="portabilidad">
-    <iframe src="https://aplicaciones.claro.com.pe/ClienteLineasWeb/consultar.htm" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
-    <iframe src="https://mi.entel.pe/portal/server.pt?&uuID={8650ACC7-3803-625E-B1C8-86A46A111000}&parentname=CommunityPage&parentid=113&mode=2&in_hi_userid=200&cached=false" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
-    <iframe name="bitel" class="bitel" src="https://portal.bitel.com.pe/asistencia/consulta-linea" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms"></iframe>
-</div>`;
-})
-btnInicio.addEventListener('click', function (e) {
-    rootContent.innerHTML = `<h1>Plataforma en Construcción</h1>`;
-})
+// btnPortabilidad.addEventListener('click', function (e) {
+//     rootContent.innerHTML = `            <div class="portabilidad">
+//     <iframe src="https://aplicaciones.claro.com.pe/ClienteLineasWeb/consultar.htm" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
+//     <iframe src="https://mi.entel.pe/portal/server.pt?&uuID={8650ACC7-3803-625E-B1C8-86A46A111000}&parentname=CommunityPage&parentid=113&mode=2&in_hi_userid=200&cached=false" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms" scrolling="no"></iframe>
+//     <iframe name="bitel" class="bitel" src="https://portal.bitel.com.pe/asistencia/consulta-linea" frameborder="0" sandbox="allow-same-origin allow-scripts allow-forms"></iframe>
+// </div>`;
+// })
+// btnInicio.addEventListener('click', function (e) {
+//     rootContent.innerHTML = `<h1>Plataforma en Construcción</h1>`;
+// })
